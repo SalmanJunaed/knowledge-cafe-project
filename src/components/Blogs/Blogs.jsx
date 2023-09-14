9bdbf36dfb819a39dfe0aa27ac6d862eedd8d7da
SalmanJunaed <salman9blog@gmail.com>
2023-09-15 02:35:41 +0600

    created blog environtment with json value

---
+++ src/components/Blogs/Blogs.jsx
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+const Blogs = () => {
+    const [blogs, setBlog] = useState([]);
+
+    useEffect(() => {
+        fetch('blogs.json')
+        .then(res => res.json())
+        .then(data => setBlog(data))
+    }, [])
+    return (
+        <div>
+            {
+                
+            }
+        </div>
+    );
+};
+
+export default Blogs;
